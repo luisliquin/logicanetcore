d291a50b1b1d302c80ec63fcbd7cf2601886c5b4
luisliquin <luisliquin@outlook.com.ar>
2024-03-01 23:31:55 -0300

    git ignore

--- LogicaNetcore/LogicaNetcore/Program.cs
+++ LogicaNetcore/LogicaNetcore/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ValidationLibrary;
+
+string[] fracciones = { "4/6", "10/11", "100/400" };
+string[] nombres = { "E. Poe", "E. A. Poe", "Edgard A. Poe", "Edgard", "e. Poe", "E Poe", "E. Allan Poe", "E. A. P.", "Edg. A. Poe" };
+
+Console.WriteLine("Simplificación de Fracciones y Validación de Nombres");
+
+foreach (var fraccion in fracciones)
+{
+    Console.WriteLine($"Simplificar {fraccion} = {FractionSimplifier.Simplify(fraccion)}");
+}
+
+foreach (var nombre in nombres)
+{
+    Console.WriteLine($"ValidName(\"{nombre}\") ➞ {NameValidator.ValidName(nombre)}");
+}
--- LogicaNetcore/LogicaNetcore/obj/Debug/net6.0/LogicaNetcore.AssemblyInfo.cs
+++ LogicaNetcore/LogicaNetcore/obj/Debug/net6.0/LogicaNetcore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogicaNetcore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b11be9c370459a13bf82e4cccdad1b594781f4f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+800959a9b453f7cd931a27b9995f8fd79bed2433")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogicaNetcore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogicaNetcore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
